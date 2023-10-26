a14d4642a49f293e7ae89bde01571156c57e30c4
Vinicius loch <viniciusdrloch@gmail.com>
2023-10-26 20:53:02 -0300

    v2

--- components/MaxMin.jsx
+++ components/MaxMin.jsx
@@ -4,18 +4,18 @@ import { Text, StyleSheet } from 'react-native';
 const MaxMin = (props) => {
   const { x, y } = props;
 
-  let message;
+  let mensagem;
 
   if (x > y) {
-    message = `Valor ${x} é maior que valor ${y}`;
+    mensagem = `Valor ${x} é maior que valor ${y}`;
   } else if (x < y) {
-    message = `Valor ${y} é maior que valor ${x}`;
+    mensagem = `Valor ${y} é maior que valor ${x}`;
   } else {
-    message = `Valor ${x} e ${y} são iguais`;
+    mensagem = `Valor ${x} e ${y} são iguais`;
   }
 
   return (
-    <Text style={styles.container}>{message}</Text>
+    <Text style={styles.container}>{mensagem}</Text>
   );
 }
 
